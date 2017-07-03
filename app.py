9c3a056ca28068bab4bccec5044166bfd6000721
MK1230 <wkl231906@gmail.com>
2017-07-04 07:51:03 +0800

    simplest flask instance and create requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'My Blog Instance'
+
+
+if __name__ == '__main__':
+    app.run()
