80248dd1b5ac5ecad9079d8e9f4ca8ce5cce22dd
MK1230 <wkl231906@gmail.com>
2017-07-05 07:34:29 +0800

    templates_base, index

--- app.py
+++ app.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return 'My Blog Instance'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
