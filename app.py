cf4169b9acef995c53c27def40605a0c0cd572c7
MK1230 <wkl231906@gmail.com>
2017-07-06 06:32:55 +0800

    flask_moment, 404.html, update requirements

--- app.py
+++ app.py
@@ -4,14 +4,27 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
