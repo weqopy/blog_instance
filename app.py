79c9b74423819b57ae2b45bf39c93476a1f60929
MK1230 <wkl231906@gmail.com>
2017-07-06 07:22:22 +0800

    flask_wtf, redirect

--- app.py
+++ app.py
@@ -2,19 +2,35 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm  # Form 已弃用
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a secret_key string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    """docstring for NameForm"""
+    name = StringField("What's your name?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    # name = None  # 初始置 name 为空，使用 session 时可忽略，查找为空时自动返回 None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        # form.name.data = ''
+        return redirect(url_for('index'))  # 重定向至 index
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 @app.route('/about/')
