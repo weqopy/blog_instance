26508cfee6591d95b748aaf0e75c8f6c1b1cc766
MK1230 <wkl231906@gmail.com>
2017-07-08 19:52:44 +0800

    add todolist; update wtform-1

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm  # Form 已弃用
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a secret_key string'
@@ -18,7 +18,12 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     """docstring for NameForm"""
-    name = StringField("What's your name?", validators=[Required()])
+    name = StringField("What's your name?", validators=[DataRequired()])
+    password = PasswordField('Set your password.',
+                             validators=[DataRequired(),
+                                         EqualTo('confirm_password',
+                                         message='Passwords must match.')])
+    confirm_password = PasswordField('Confirm the password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -27,14 +32,17 @@ def index():
     # name = None  # 初始置 name 为空，使用 session 时可忽略，查找为空时自动返回 None
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')  # 先取出 session 中保存的 name 变量，再进行对比
+        # 先取出 session 中保存的 name 变量，再进行对比
+        old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Oh, you changed your name!', category='info')
         else:
             flash('Hello again.', category='info')
         session['name'] = form.name.data
+        session['password'] = form.password.data
         # form.name.data = ''
-        return redirect(url_for('index'))  # 重定向至 index
+        # 重定向至 index
+        return redirect(url_for('index'))
     return render_template('index.html',
                            current_time=datetime.utcnow(),
                            form=form,
