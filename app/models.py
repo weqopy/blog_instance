6cc9451f0048f35dc2635a09b8532fd34df75519
MK1230 <wkl231906@gmail.com>
2017-07-28 07:34:00 +0800

    rebuild db and adjust small copies

--- app/models.py
+++ app/models.py
@@ -52,7 +52,8 @@ class User(UserMixin, db.Model):
         if self.role is None:
             if self.email == current_app.config['FLASKY_ADMIN']:
                 self.role = Role.query.filter_by(permissions=0xff).first()
-            self.role = Role.query.filter_by(default=True).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
 
     @property
     def password(self):
